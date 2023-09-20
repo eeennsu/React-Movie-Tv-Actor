8338a972573235a61d3c7a1c2bd1c882642f6245
eeennsu <116697733+eeennsu@users.noreply.github.com>
2023-09-20 14:44:18 +0900

    Arrow Button 살짝 수정

--- src/components/ArrowButton.tsx
+++ src/components/ArrowButton.tsx
@@ -8,15 +8,15 @@ interface Props extends CustomArrowProps{
 
 const ArrowButton: FC<Props> = ({ position, onClick }) => {
 
-    const _className = `${position === 'left' ? 'left-0 -translate-x-4 -translate-y-1/2' : 'right-0 translate-x-4 -translate-y-1/2' }`;
+    const _className = `${position === 'left' ? 'left-0 -translate-x-7' : 'right-0 translate-x-7' }`;
 
     return (
-        <button onClick={onClick} className={`absolute z-10 top-1/2 ${_className}`} >
+        <button onClick={onClick} className={`absolute z-10 top-1/2 -translate-y-9 ${_className}`} >
             {
                 position === 'left' ? (
-                    <ArrowLeftOutlined className='p-3 text-xl bg-yellow-100 rounded-full shadow-lg hover:text-yellow-100 hover:bg-slate-700 text-slate-700' />
+                    <ArrowLeftOutlined className='p-3 text-xl text-white transition-colors bg-teal-500 rounded-full shadow-lg md:p-4 hover:text-yellow-500 hover:bg-slate-700' />
                 ) : (
-                    <ArrowRightOutlined className='p-3 text-xl bg-yellow-100 rounded-full shadow-lg hover:text-yellow-100 hover:bg-slate-700 text-slate-700' />
+                    <ArrowRightOutlined className='p-3 text-xl text-white transition-colors bg-teal-500 rounded-full shadow-lg md:p-4 hover:text-yellow-500 hover:bg-slate-700' />
                 )
             }
         </button>
